fbfaef8cfdc527cc67254ed5cdb8cd45603202a4
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-27 12:43:48 +0200

    abbrev

---
+++ os_scripts/abbreviations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.8
+
+import argparse
+
+
+def get_input():
+    # my_parser = argparse.ArgumentParser(allow_abbrev=False)
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument('--input', action='store', type=int, required=True)
+    my_parser.add_argument('--id', action='store', type=int)
+
+    args = my_parser.parse_args()
+    return args.input
+
+
+print(get_input())
